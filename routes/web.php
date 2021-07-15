59c165ff37e31ec0ecf33689e200b4f3f55d4a71
ZenAyaka <zenayaka97.0121@gmail.com>
2021-07-15 07:41:58 +0000

    kadai08

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['profix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');});
+    
+    Route::get('admin/profile/edit', 'Admin\ProfileController@add');
+    //adminは絶対なのか？adminて何？
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+    
